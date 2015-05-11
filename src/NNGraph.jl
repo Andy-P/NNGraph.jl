84b148a90a5d63062ae7f1e2e0fc61eae8a28969
Andre P. <apemmelaar@gmail.com>
2015-05-11 19:28:02 +0900

    Update and rename RecurrentNN.jl to NNGraph.jl

--- src/NNGraph.jl
+++ src/NNGraph.jl
@@ -1,15 +1,11 @@
-module RecurrentNN
+module NNGraph
 import Base.tanh
-
-export Model, RNN, LSTM
 export NNMatrix, randNNMat, forwardprop, softmax, Solver, step
 export Graph, backprop, rowpluck
 
 include("recurrent.jl")
 include("graph.jl")
 include("solver.jl")
-include("rnn.jl")
-include("lstm.jl")
 
 
 end # module
