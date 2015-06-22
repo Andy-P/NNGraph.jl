6b994746912dfa2229850f57eefc21c8fbedb98d
Andy-P <apemmelaar@gmail.com>
2015-06-22 11:08:50 +0900

    Adding several new function to graph.jl
    1. Added concat() to merge two matrices
    2. Modified add() to use varargs
    2. Added add() single constant

--- src/graph.jl
+++ src/graph.jl
@@ -22,6 +22,34 @@ function rowpluck(g::Graph, m::NNMatrix, ix::Int)
     return out
 end
 
+function concat(g::Graph, ms::NNMatrix...)
+    n = 0
+    @inbounds for i in 1:length(ms)
+        n += ms[i].n
+    end
+    out = NNMatrix(n, ms[1].d, zeros(n, ms[1].d), zeros(n, ms[1].d))
+    n = 0
+    @inbounds for m in ms
+        for j = 1:m.d, i = 1:m.n
+            out.w[i+n,j] = m.w[i,j]
+        end
+        n += m.n
+    end
+    if g.doBackprop
+        push!(g.backprop,
+              function ()
+                  n = 0
+                  @inbounds for m in ms
+                      @inbounds for j = 1:m.d, i = 1:m.n
+                          m.dw[i,j] += out.dw[i+n,j]
+                      end
+                      n += m.n
+                  end
+              end )
+    end
+    return out
+end
+
 function tanh(g::Graph, m::NNMatrix)
     out = NNMatrix(m.n, m.d)
     out.w = tanh(m.w)
@@ -85,22 +113,49 @@ function mul(g::Graph, m1::NNMatrix, m2::NNMatrix)
     return out
 end
 
-function add(g::Graph, m1::NNMatrix, m2::NNMatrix)
-    out = NNMatrix(m1.n, m1.d, m1.w .+ m2.w, zeros(m1.n, m1.d))
+function mul(g::Graph, m::NNMatrix, c::Float64)
+    out = NNMatrix(m.n, m.d, m.w .* c, zeros(m.n, m.d))
     if g.doBackprop
         push!(g.backprop,
             function ()
-                @inbounds for j in 1:m1.d, i in 1:m1.n
-                  m1.dw[i,j] += out.dw[i,j]
-                  m2.dw[i,j] += out.dw[i,j]
+                m.dw += out.dw .* c
+            end )
+    end
+    return out
+end
+
+function add(g::Graph, ms::NNMatrix...)
+    out = NNMatrix(ms[1].n, ms[1].d, zeros(ms[1].n, ms[1].d), zeros(ms[1].n, ms[1].d))
+    @inbounds for m in ms
+        @inbounds for j in 1:m.d, i in 1:m.n
+            out.w[i,j] += m.w[i,j]
+        end
+    end
+    if g.doBackprop
+        push!(g.backprop,
+            function ()
+                @inbounds for m in ms
+                    @inbounds for j in 1:m.d, i in 1:m.n
+                        m.dw[i,j] += out.dw[i,j]
+                    end
                 end
             end )
     end
     return out
 end
 
-function eltmul(g::Graph, m1::NNMatrix, m2::NNMatrix) # element-wise multiplication
+function add(g::Graph, m::NNMatrix, c::Float64)
+    out = NNMatrix(m.n, m.d, m.w .+ c, zeros(m.n, m.d))
+    if g.doBackprop
+        push!(g.backprop,
+            function ()
+                m.dw += out.dw
+            end )
+    end
+    return out
+end
 
+function eltmul(g::Graph, m1::NNMatrix, m2::NNMatrix) # element-wise multiplication
     out = NNMatrix(m1.n, m2.d, m1.w .* m2.w, zeros(m1.n, m2.d))
     if g.doBackprop
         push!(g.backprop,
@@ -109,23 +164,7 @@ function eltmul(g::Graph, m1::NNMatrix, m2::NNMatrix) # element-wise multiplicat
                   m1.dw[i,j] += m2.w[i,j] * out.dw[i,j]
                   m2.dw[i,j] += m1.w[i,j] * out.dw[i,j]
                 end
-            end )
-    end
-    return out
-end
-
-function dot(g::Graph, m1::NNMatrix, m2::NNMatrix)
-
-    out = NNMatrix(1, 1, zeros(1,1), zeros(1,1))
-    out.w[1,1] = dot(m1.w, m2.w)
-    if g.doBackprop
-        push!(g.backprop,
-            function ()
-                @inbounds for j in 1:m1.d, i in 1:m1.n
-                    m1.dw[i,j] += m2.w[i,j] * out.dw[1,1]
-                    m2.dw[i,j] += m1.w[i,j] * out.dw[1,1]
-                end
-            end )
-    end
+              end )
+        end
     return out
 end
